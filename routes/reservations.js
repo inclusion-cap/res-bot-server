fb3f0e400605bc5ff81303cc4cb0fc25b2063ccb
Pavel Machuca <pavelmachuca@hotmail.com>
2019-07-22 15:45:46 -0400

    testing response to /reserve command

--- routes/reservations.js
+++ routes/reservations.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV !== 'production') {
       slack = require('../secrets.json').slack;
 } else {
       slack = {
-            one: process.env.SLACK_ONE,
+            one: process.env.TEAM_ID,
             two: process.env.SLACK_TWO,
             three: process.env.SLACK_THREE,
       }
@@ -24,8 +24,8 @@ router.post('/slack-confirmation', function (req, res, next) {
 router.post('/slack-hello', function (req, res, next) {
       (async () => {
             try {
-                  await axios.post(`https://hooks.slack.com/services/${slack.one}/${slack.two}/${slack.three}`, {
-                        "text": "second message"
+                  await axios.post(`https://hooks.slack.com/services/${slack.TEAM_ID}/${slack.two}/${slack.three}`, {
+                        "text": "hello message"
                   });
 
             } catch (e) {
@@ -37,6 +37,27 @@ router.post('/slack-hello', function (req, res, next) {
 });
 
 router.post('/slack-reservation', function (req, res, next) {
+      let text = req.body.text.split(' ');
+      let obj = {};
+
+      for (let i = 0; i < text.length; i++) {
+            if (text[i] === 'for') {
+                  obj.guests = text[i + 1];
+            }
+            if (text[i] === 'at') {
+                  obj.time = text[i + 1];
+            }
+      }
+
+      (async () => {
+            try {
+                  await axios.post(`https://hooks.slack.com/services/${slack.TEAM_ID}/${slack.two}/${slack.three}`, {
+                        "text": `yas queen, got your table for ${obj.guests} at ${obj.time}`
+                  });
+            } catch (e) {
+                  console.error(e);
+            }
+      })();
       console.log('request to slack reservation\n\n', req.body);
 });
 
